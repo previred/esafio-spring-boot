fcef5f71eee02600f8b9ce0824b4d4f228338f09
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-02-12 23:30:07 -0500

    Se quito un micro que no se usaba

--- backend/security/src/main/java/com/moveapps/security/user/infraestructure/adapters/IUserAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.moveapps.security.user.infraestructure.adapters;
-
-import com.moveapps.security.user.domains.data.UserDTO;
-import com.moveapps.security.user.infraestructure.entities.UserEntity;
-
-public interface IUserAdapter {
-	UserEntity toEntity(UserDTO userDTO);
-}
