9b8d7a04279c0f62d39f96293b47bc16ce1d4591
FerC <149102084+DaRialCut@users.noreply.github.com>
2024-02-22 16:31:44 -0300

    configuracion de API First

---
+++ src/test/java/com/springboot/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
